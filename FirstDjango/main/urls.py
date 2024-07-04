0fa45be711a2d0afb146fe496082d62f731deb67
SergBo77 <seman77@mail.ru>
2024-07-04 16:11:19 +0300

    add links

--- FirstDjango/main/urls.py
+++ FirstDjango/main/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('start', views.start),
+    path('contacts', views.contacts),
+    path('registration', views.registration)
 ]
--- FirstDjango/main/views.py
+++ FirstDjango/main/views.py
@@ -5,3 +5,12 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("<h1>Это мой первый проект на Django</h1>")
+
+def start(request):
+    return HttpResponse("<h1>Это страница start на Django</h1>")
+
+def contacts(request):
+    return HttpResponse("<h1>Это  страница contacts на Django</h1>")
+
+def registration(request):
+    return HttpResponse("<h1>Это  страница регистрации на Django</h1>")
