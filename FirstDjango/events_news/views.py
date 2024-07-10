5cab33082595a7a07d901ccc628e55eac169d4d8
SergBo77 <seman77@mail.ru>
2024-07-10 13:07:54 +0300

    css events

--- FirstDjango/events_news/views.py
+++ FirstDjango/events_news/views.py
@@ -3,5 +3,9 @@ from .models import events_news
 
 # Create your views here.
 def home(request):
+
     events = events_news.objects.all()
-    return render(request, 'events_news/events_news.html', {'events': events})
+    context = { 'events': events,
+        'background_image': 'events_news/img/interior-design-yoga-space.jpg'
+    }
+    return render(request, 'events_news/events_news.html', context)
